3088a92ed39ec8e5be64f71717d7fd6ce540874b
ShaikAbdulWahab <abdulwahab.shaik@modlix.com>
2024-08-28 16:12:29 +0530

    feature/removing-comments

--- phase1/AbdulWahab/PythonProject(LoadCSVtoSQL)/functionality.py
+++ phase1/AbdulWahab/PythonProject(LoadCSVtoSQL)/functionality.py
@@ -1,26 +1,19 @@
 from sqlalchemy import MetaData, Table, Column, Integer, String, Float, BigInteger, \
   DateTime, TEXT
-#from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.exc import SQLAlchemyError
 import pandas as pd
 def loadData(engine):
-    # Specify the paths of files you want to iterate over
     files=['Archive/activity.csv','Archive/opportunity.csv','Archive/opportunity_latest_activities.csv']
-    #Specify the table names
     tables=['activity','opportunity','opportunity_latest_activity']
-    # Iterate over the files in the directory
     count=0
     for file in files:
-        # Define the name of the table in MySQL
         table_name = tables[count]
         count+=1
  
-        # Read a small chunk of the CSV to infer column types
         sample_chunk = pd.read_csv(file, nrows=1000)
-                # Define table schema based on the CSV columns
+            
         metadata = MetaData()
  
-        # You can customize the columns as needed
         columns = []
         for col_name in sample_chunk.columns:
             col_data = sample_chunk[col_name]
@@ -41,23 +34,20 @@ def loadData(engine):
             elif pd.api.types.is_datetime64_any_dtype(col_data) and not col_data.isna().all():
                 columns.append(Column(col_name, DateTime))
             else:
-                # Default to String for other types
                 columns.append(Column(col_name, String(255)))
        
-        # Create the table
         table = Table(table_name, metadata, *columns)
        
-        # Create the table in the database if it doesn't exist
         try:
             metadata.create_all(engine)
             print(f"Table '{table_name}' is created or already exists.")
         except SQLAlchemyError as e:
             print(f"Error creating table: {e}")
        
-        # Specify the chunk size (number of rows per chunk)
+
         chunk_size = 20000
         i = 0
-        # Load CSV in chunks and insert into the MySQL table
+
         for chunk in pd.read_csv(file, chunksize=chunk_size):
             try:
                 chunk.to_sql(name=table_name, con=engine, if_exists='append', index=False)
@@ -67,4 +57,3 @@ def loadData(engine):
                 print(f"Error inserting data: {e}")
                 break
  
-    print("Finished loading the all CSV files into the MySQL database.")
--- phase1/AbdulWahab/PythonProject(LoadCSVtoSQL)/main.py
+++ phase1/AbdulWahab/PythonProject(LoadCSVtoSQL)/main.py
@@ -1,13 +1,11 @@
 from urllib.parse import quote
 from sqlalchemy import create_engine 
 from functionality import loadData
-# MySQL database credentials
 user = 'root'
 password = quote("Abdul@954")
-host = 'localhost'  # e.g., 'localhost' or '127.0.0.1'
+host = 'localhost'  
 database = 'fincity2'
- 
-# Create a connection to the MySQL database
+
 engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
 
 loadData(engine)
