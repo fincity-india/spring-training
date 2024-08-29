fd1f0f637ca2031a20cec57259e1f7a6041785ed
Wahab-Fincity <abdulwahab.shaik@modlix.com>
2024-08-29 16:31:50 +0530

    feature/update-main.py

--- phase1/AbdulWahab/PythonProject(LoadCSVtoSQL)/main.py
+++ phase1/AbdulWahab/PythonProject(LoadCSVtoSQL)/main.py
@@ -1,23 +1,37 @@
 from urllib.parse import quote
 from sqlalchemy import MetaData,create_engine 
-from functionality import loadData,createTable,addChunksToSql
+from functionality import load_data,create_table,add_chunks_to_sql
 import os
+import logging
+from pathlib import Path
 
-main_folder = 'files'
-folders = [name for name in os.listdir(main_folder) if os.path.isdir(os.path.join(main_folder, name))]
 
-user = 'root'
-password = quote("Abdul@954")
-host = 'localhost'  
-database=folders[0]
-database_path=main_folder+"/"+database
-files=[database_path+"/"+name for name in os.listdir(database_path)]
-engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
-metadata = MetaData()
-chunk_size=15000
+def main():
+    user = '********'
+    password = quote("********")
+    host = '********' 
+    main_folder = 'files'
+    main_folder_path = Path(main_folder)
+    print(main_folder)
+    if not main_folder_path.exists():
+        logging.error(f"The directory '{main_folder}' does not exist.")
+    folders = [folder for folder in main_folder_path.iterdir() if folder.is_dir()]
+    print(folders)
+    if not folders:
+        logging.error("No subdirectories found in the specified main folder.")  
+    database = os.path.basename(folders[0])
+    print(database)
+    files = [str(file) for file in folders[0].iterdir() if file.is_file()]
+    print(files)
+    engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
+    metadata = MetaData()
+    chunk_size=15000
 
-for file in files:
-    table_name=file.split("/")[-1].split(".")[0]
-    loadData(file,table_name,metadata)
-    createTable(engine,metadata,table_name)
-    addChunksToSql(engine,file,table_name,chunk_size)
+    for file in files:
+        table_name = os.path.splitext(os.path.basename(file))[0]
+        load_data(file,table_name,metadata)
+        create_table(engine,metadata,table_name)
+        add_chunks_to_sql(engine,file,table_name,chunk_size)
+
+if __name__ == "__main__":
+    main()
