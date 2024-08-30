074f7881829dddbacaca15f96ca3add1c1606567
Imthiyaz <syed.imthiyaz@modlix.com>
2024-08-30 11:06:39 +0530

    feature/added-code-to-create-database-with-folder-name

--- phase1/Imthiyaz/utils.py
+++ phase1/Imthiyaz/utils.py
@@ -1,6 +1,8 @@
 import logging
 import os
 from pathlib import Path
+import mysql.connector
+from mysql.connector import Error
 import pandas as pd
 from sqlalchemy import MetaData, Table, create_engine
 from sqlalchemy.exc import SQLAlchemyError
@@ -13,10 +15,10 @@ load_dotenv()
 user = os.getenv('USER')
 password = os.getenv('PASSWORD')
 host = os.getenv('HOST')
+database = 'databasename'
 
 
 def engine_creator():
-    database = 'FincityData'
     engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
     return engine
 
@@ -55,5 +57,34 @@ def get_table_name():
         raise FileNotFoundError("No subdirectories found in the specified main folder.")
     database = folders[0] 
     files = [str(file) for file in database.iterdir() if file.is_file()]
+    # database_name = database.name.split('\\')
+    # return database_name[0]
     return files
+
+
+# def create_database(database_name):
+#     try:
+#         connection = mysql.connector.connect(
+#             host= host,
+#             user= user, 
+#             password= password 
+#             )
+        
+#         if connection.is_connected():
+#             logging.info('Connected to MySQL server')
+
+#             cursor = connection.cursor()
+#             create_db_query = f"CREATE DATABASE {database_name}"
+#             cursor.execute(create_db_query)
+#             logging.info(f"Database `{database_name}` created successfully")
+
+#     except Error as e:
+#         logging.error(f"Error: {e}")
+
+#     finally:
+#         if connection.is_connected():
+#             cursor.close()
+#             connection.close()
+#             logging.info("MySQL connection is closed")
+
     
