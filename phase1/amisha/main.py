21b037cfc3a68a2e50ffdc1e1205cc0e03b7f246
imthiyaz348 <syed.imthiyaz@modlix.com>
2024-09-02 17:50:53 +0530

    Revert "feature/to-create-new-database-and-import-csvfiles"

--- phase1/amisha/main.py
+++ phase1/amisha/main.py
@@ -1,11 +1,10 @@
 import os
-from utils import create_table, engine_creator, get_table_name, insert_data, create_database
+from utils import create_table, engine_creator, get_table_name, insert_data
 
 
 def main():
     engine = engine_creator()
-    files, database_name = get_table_name()
-    create_database(database_name)
+    files = get_table_name()
     for file in files:
         table_name = os.path.splitext(os.path.basename(file))[0]
         create_table(file, table_name, engine)
