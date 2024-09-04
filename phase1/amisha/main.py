b9a95b6056b362f2399b4730578b4ab6821c9294
Imthiyaz <syed.imthiyaz@modlix.com>
2024-09-04 09:40:11 +0530

    feature/changes-done

--- phase1/amisha/main.py
+++ phase1/amisha/main.py
@@ -1,17 +1,12 @@
-from sqlalchemy import create_engine
-from utils import create_table, get_table_name, insert_data
+import os
+from utils import create_table, engine_creator, get_table_name, insert_data
 
 
 def main():
-    user = 'root'
-    password = 'root'
-    host = 'localhost'
-    database = 'FincityData3'
-    engine = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{database}')
-    
+    engine = engine_creator()
     files = get_table_name()
     for file in files:
-        table_name=file.split("\\")[-1].split(".")[0]
+        table_name = os.path.splitext(os.path.basename(file))[0]
         create_table(file, table_name, engine)
         insert_data(file,table_name,engine)
 
